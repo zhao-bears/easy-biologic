e88a5271c166f368cad837e2017b93f81aec17f2
zhao <20024592+agentzhao@users.noreply.github.com>
2026-02-24 10:11:48 +0800

    refactor: version defined in pyproject instead

--- src/puda_biologic/__about__.py
+++ src/puda_biologic/__about__.py
@@ -1,1 +1,6 @@
-__version__ = "0.5.0rc1"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("puda-biologic")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
